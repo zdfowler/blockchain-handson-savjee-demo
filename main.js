2d20bc75a05a6a10c62ca566d65bacfa9d3fd782
Zac <zfowler@unomaha.edu>
2018-03-27 23:25:07 -0500

    Spelling

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const SHA256 = require('crypto-js/sha256')
 
 class Block {
-    construtor (index, timestamp, data, previousHash = '') {
+    constructor (index, timestamp, data, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
@@ -20,23 +20,21 @@ class Blockchain {
 
     }
     createGenesisBlock(){
-        return new Block("0","01/01/2018",{description: "Genesis Block"},0);
+        return new Block(0,"01/01/2018","Genesis Block","0");
     }
-    getLastestBlock() {
+    getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
     addBlock(newBlock) {
-        newBlock.previousHash = this.getLastestBlock().hash;
-        console.log("Just added hash " + newBlock.previousHash);
+        newBlock.previousHash = this.getLatestBlock().hash;
         newBlock.hash = newBlock.calculateHash();
-
         // Normally you'd have other checks here.
         this.chain.push(newBlock);
     }
 }
 
 let zacCoin = new Blockchain();
-zacCoin.addBlock(new Block(1,"03/26/2018",{amount: 4}));
-zacCoin.addBlock(new Block(2,"03/27/2018",{amount: 6}));
+zacCoin.addBlock(new Block(1,"03/27/2018", {amount: 4}));
+zacCoin.addBlock(new Block(2,"03/28/2018", {amount: 6}));
 
 console.log(JSON.stringify(zacCoin, null, 4));
